693adcf623695ae73ad406850aa6ef5f91909188
Vihanga Tharuka <vihangatd99@gmail.com>
2026-01-05 09:54:39 +0530

    initial commit

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Landing } from './landing/landing';
+import { SharedModule } from './shared/shared-module';
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Landing
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    SharedModule,
+    ReactiveFormsModule
+],
   providers: [
     provideBrowserGlobalErrorListeners()
   ],
