deaba12aa9750dc49c69537d1a628e3c48240ab3
Vihanga Tharuka <vihangatd99@gmail.com>
2026-01-14 01:13:35 +0530

    add change password

--- src/app/shared/components/change-password-dialog/change-password-dialog.ts
+++ src/app/shared/components/change-password-dialog/change-password-dialog.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth-service';
+import { NotificationService } from '../../services/notification-service';
+import { ErrorHandlerService } from '../../services/error-handler-service';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-change-password-dialog',
@@ -6,6 +11,52 @@ import { Component } from '@angular/core';
   templateUrl: './change-password-dialog.html',
   styleUrl: './change-password-dialog.css',
 })
-export class ChangePasswordDialog {
+export class ChangePasswordDialog{
+
+  changePasswordForm!:FormGroup;
+  loading=false;
+
+  hideCurrent = true;
+  hideNew= true;
+  hideConfirm=true;
+
+  constructor(
+    private fb:FormBuilder,
+    private dialogRef:MatDialogRef<ChangePasswordDialog>,
+    private authService:AuthService,
+    private notification:NotificationService,
+    private errorHandlerService:ErrorHandlerService
+  ){
+    this.changePasswordForm = this.fb.group({
+      currentPassword:['',[Validators.required]],
+      newPassword:['',[Validators.required,Validators.minLength(6)]],
+      confirmPassword:['',[Validators.required,this.authService.passwordMatchValidator('newPassword')]]
+    })
+  }
+
+  submit(){
+    this.loading = true;
+    const formData = this.changePasswordForm.value;
+    const data = {
+      currentPassword:formData.currentPassword,
+      newPassword:formData.newPassword
+    };
+
+    this.authService.changePassword(data).subscribe({
+      next:(response:any)=>{
+        this.loading = false;
+        this.notification.success(response.message || 'Password changed successfully');
+        this.dialogRef.close(true);
+      },
+      error:(err)=>{
+        this.loading = false;
+        this.errorHandlerService.handle(err,'Failed to change password. Please try again.');
+      }
+    });
+  }
+
+  cancel(){
+    this.dialogRef.close(false);
+  }
 
 }
